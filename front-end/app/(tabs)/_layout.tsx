c9a0e7be034df8196f407e0f27ce38e85ebadac4
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-12-01 22:26:43 +0200

    Initialize React Native Application With Expo + TypeScript

---
+++ front-end/app/(tabs)/_layout.tsx
@@ -0,0 +1,20 @@
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+        }}
+      />
+    </Tabs>
+  );
+}
