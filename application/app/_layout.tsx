417468f89642146e4dd40d8124977bee83ec79a3
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-12-01 23:22:15 +0200

    Use global.css For NativeWind In RootLayout

--- application/app/_layout.tsx
+++ application/app/_layout.tsx
@@ -1,9 +1,12 @@
 import { Stack } from "expo-router";
 
+// Import your global CSS file
+import "../global.css";
+
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="(onboarding)" options={{ headerShown: false }} />
     </Stack>
   );
 }
