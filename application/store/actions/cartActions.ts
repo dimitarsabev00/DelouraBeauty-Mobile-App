ce8950ce7381bb4f87504654daa471319ddf55bd
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-12-03 22:58:57 +0200

    Added Actions & Reducers For ShoppingCart

---
+++ application/store/actions/cartActions.ts
@@ -0,0 +1,19 @@
+export const addToCart = (data) => {
+  return {
+    type: "ADD_TO_CART",
+    item: data,
+  };
+};
+
+export const removeFromCart = (_id) => {
+  return {
+    type: "REMOVE_FROM_CART",
+    itemID: _id,
+  };
+};
+
+export const emptyCart = () => {
+  return {
+    type: "EMPTY_CART",
+  };
+};
--- application/store/reducers/index.ts
+++ application/store/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import feedsReducer from "./feedsReducer";
+import cartReducer from "./cartReducer";
 
 const myReducers = combineReducers({
   feeds: feedsReducer,
+  cartItems: cartReducer,
 });
 
 export default myReducers;
